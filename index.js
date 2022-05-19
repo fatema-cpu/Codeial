25799fed8d27b0f25be9c1ddfb9696b99d34ae03
Fatema <fatemahussain451@gmail.com>
2022-05-19 10:37:25 +0530

    seeting up view engine

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port=8000
 // use express router
 app.use('/',require('./routes'))
 
+// setting up view engine
+app.set('view engine','ejs')
+app.set('views','./views')
+
 app.listen(port,function(error){
     if(error){
         console.log(`error while running the server: ${error}`)
