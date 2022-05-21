66c1f65748437a1466e8c4580abb5a093292ecc5
Fatema <fatemahussain451@gmail.com>
2022-05-21 16:44:47 +0530

    create sign-in through manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,10 +1,20 @@
 const User=require('../models/user')
 
 module.exports.profile=function(req,res){
-    res.render('user_profile',{
-        title:"user-profile"
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id,function(err,user){
+            if(user){
+                return res.render('user_profile',{
+                    title:'User profile',
+                    user:user
+                })
+            }
+            return res.redirect('/users/sign-in')
+        })
     }
-    )
+    else{
+        return res.redirect('/users/sign-in')
+    }
 }
 
 // render the sign up page
@@ -48,5 +58,29 @@ module.exports.create=function(req,res){
 
 // sign in and create a session for the user
 module.exports.createSession=function(req,res){
-    
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log("error in finding user in sign in!!")
+            return
+        }
+        // handle user found
+        if(user){
+            // handle password which doesn't match
+            if(user.password!=req.body.password){
+                return res.redirect('back')
+            }
+            // handle session creation
+            res.cookie('user_id',user.id)
+            return res.redirect('/users/profile')
+        }
+        // handle user not found
+        else{
+            return res.redirect('back')
+        }
+    })
+}
+
+module.exports.signOut=function(req,res){
+    res.cookie('user_id','')
+    res.redirect('/users/sign-in')
 }
--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,7 @@ router.get('/profile',userController.profile)
 router.get('/sign-up',userController.signUp)
 router.get('/sign-in',userController.signIn)
 router.post('/create',userController.create)
+router.post('/create-session',userController.createSession)
+router.post('/sign-out',userController.signOut)
 
 module.exports=router
