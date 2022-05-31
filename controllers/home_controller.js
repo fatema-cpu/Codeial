9bb6b8dbd52c189fb1e7b7168dd0414375b77481
Fatema <fatemahussain451@gmail.com>
2022-05-31 15:30:24 +0530

    display post with username on home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,11 +3,18 @@ const Post=require('../models/post')
 module.exports.home=function(req,res){
     // console.log(req.cookies)
     // res.cookie('id',22)
-    Post.find({},function(err,posts){
+    // Post.find({},function(err,posts){
+    //     res.render('home',{
+    //         title:"Codeial | home",
+    //         posts:posts
+    //     })
+    // })
+
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
         res.render('home',{
             title:"Codeial | home",
             posts:posts
         })
     })
-    
 }
