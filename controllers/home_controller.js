50b36d04757f20a2fb87d73b8891e653b786326b
Fatema <fatemahussain451@gmail.com>
2022-05-31 15:07:37 +0530

    display posts on home screen

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,13 @@
+const Post=require('../models/post')
+
 module.exports.home=function(req,res){
-    console.log(req.cookies)
-    res.cookie('id',22)
-    res.render('home',{
-        title:"home"
+    // console.log(req.cookies)
+    // res.cookie('id',22)
+    Post.find({},function(err,posts){
+        res.render('home',{
+            title:"Codeial | home",
+            posts:posts
+        })
     })
+    
 }
